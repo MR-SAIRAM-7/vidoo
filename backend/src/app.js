f2809f99ca08766872f94648082566a7efe07d5c
sairam <mr.sairamofficial@gmail.com>
2025-09-10 10:56:54 +0530

    creating register and login routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,16 @@ import { Server } from "node:http";
 import mongoose from "mongoose";
 import cors from "cors";
 import connectToSocket from "./controllers/socketManager.js";
-
-
+import userRoutes from "./routes/users.routes.js"
 const app = express();
 const server = new Server(app);
 const io = connectToSocket(server)
 const port = 8000;
 app.use(cors());
-app.use(express.json({ limit: "40kb" }));
+app.use(express.json({ limit: "40kb" }))
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.set("port", process.env.PORT || 8000);
+app.use("/api/v1/users",userRoutes);
 
 
 // app.get("/test", (req, res) => {
