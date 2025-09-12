d8299935054aa40ba67b2f7da96c13363cec6950
sairam <mr.sairamofficial@gmail.com>
2025-09-12 13:18:08 +0530

    creating sockets for connections

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,9 +1,88 @@
 import { Server } from "socket.io"
 
 
-const connectToSocket = (server) =>{
+let connections = {}
+let messages = {}
+let timeOnline = {}
+
+
+const connectToSocket = (server) => {
     const io = new Server(server);
-    return io;
+
+    io.on("connection", (socket) => {
+
+        socket.on("join-call", (path) => {
+            if (connections[path] === undefined) {
+                connections[path] = []
+            }
+
+            connections[path].push(socket.id)
+            timeOnline[socket.id] = new Date();
+
+            for (let a = 0; a < connections[path].length; a++) {
+                io.to(connections[path][a]).emit("user-joined", socket.id);
+            }
+
+            if (messages[path] === undefined) {
+                for (let a = 0; a < messages[path].length; ++a) {
+                    io.to(socket.id).emit("chat-message", messages[path][a]['data'], messages[path][a]['sender'], messages[path][a]['socket-id-sender']);
+                }
+            }
+
+        })
+
+        socket.on("signal", (toId, message) => {
+            io.to(toId).emit("signal", socket.id, message)
+        })
+
+        socket.on("chat-message", (data, sender) => {
+            const [matchingRomm, found] = Object.entries(connections).reduce(([room, isFound, roomKey, roomValue]) => {
+                if (!isFound && roomValue.includes(socket.id)) {
+                    return [roomKey, true];
+                }
+                return [room, isFound];
+            }, ['', false])
+            if (found === true) {
+                if (messages[matchingRomm] === undefined) {
+                    messages[matchingRomm] = []
+                }
+                messages[matchingRomm].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id });
+                console.log("message", key, ":", sender, data);
+                connections[matchingRomm].forEach(element => {
+                    io.to(element).emit("chat-message", data, sender, socket.id);
+                });
+            }
+        });
+
+
+
+        socket.on("disconnect", () => {
+            var diffTime = Math.abs(timeOnline[socket.id] - new Date());
+
+            var key;
+
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+                for (let a = 0; a < v.length; ++a) {
+                    if (v[a] === socket.id) {
+                        key = k;
+
+                        for (let a = 0; a < connections[key].length; ++a) {
+                            io.to(connections[key][a]).emit('user-left', socket.id);
+                        }
+
+                        var index = connections[key].indexOf(socket.id);
+
+                        connections[key].splice(index, 1);
+
+
+                        if (connections[key].length === 0) {
+                            delete connections[key];
+                        }
+                    }
+                }
+            }
+        })
+    })
 }
 
 export default connectToSocket;
